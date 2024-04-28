17399a8b7329d2329f77aa920fee186f63850fc4
seanwillmis <36089366+seanwillmis@users.noreply.github.com>
2024-04-28 14:27:56 -0400

    Exercise 2.3

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -27,12 +27,13 @@ const Content = (props) => {
   );
 };
 
-const Total = (props) => {
-  // console.log(props.part1);
+const Total = ({ parts }) => {
+  var total = parts.reduce((accum, item) => accum + item.exercises, 0);
+
   return (
     <div>
       {/* 2. then define the logic */}
-      <p>Number of exercises {props.part1 + props.part2 + props.part3}</p>
+      <p>Number of exercises {total}</p>
     </div>
   );
 };
@@ -47,36 +48,60 @@ const Course = ({ course }) => {
         part3={course.parts[2]}
       />
       <Total
-        part1={course.parts[0].exercises} // 1. define the parameters here first
-        part2={course.parts[1].exercises}
-        part3={course.parts[2].exercises}
+        parts={course.parts} // 1. define the parameters here first
       />
     </div>
   );
 };
 
 const App = () => {
-  const course = {
-    id: 1,
-    name: "Half Stack application development",
-    parts: [
-      {
-        name: "Fundamentals of React",
-        exercises: 10,
-        id: 1,
-      },
-      {
-        name: "Using props to pass data",
-        exercises: 7,
-        id: 2,
-      },
-      {
-        name: "State of a component",
-        exercises: 14,
-        id: 3,
-      },
-    ],
-  };
+  /** Define states */
+  // const [anecdote, setAnecdote] = useState(0);
+
+  const courses = [
+    {
+      name: "Half Stack application development",
+      id: 1,
+      parts: [
+        {
+          name: "Fundamentals of React",
+          exercises: 10,
+          id: 1,
+        },
+        {
+          name: "Using props to pass data",
+          exercises: 7,
+          id: 2,
+        },
+        {
+          name: "State of a component",
+          exercises: 14,
+          id: 3,
+        },
+        {
+          name: "Redux",
+          exercises: 11,
+          id: 4,
+        },
+      ],
+    },
+    {
+      name: "Node.js",
+      id: 2,
+      parts: [
+        {
+          name: "Routing",
+          exercises: 3,
+          id: 1,
+        },
+        {
+          name: "Middlewares",
+          exercises: 7,
+          id: 2,
+        },
+      ],
+    },
+  ];
 
   return <Course course={course} />;
 };
