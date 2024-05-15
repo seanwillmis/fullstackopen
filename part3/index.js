137c6aae1c6c9bc4a75a22932016bbcdf337aee8
seanwillmis <36089366+seanwillmis@users.noreply.github.com>
2024-05-15 11:47:22 -0400

    Exercise 3.7-3.8

--- part3/index.js
+++ part3/index.js
@@ -1,7 +1,13 @@
 const express = require("express");
-
+const morgan = require("morgan");
 const app = express();
+const PORT = 3001;
+
 app.use(express.json());
+morgan.token("body", (req, res) => JSON.stringify(req.body));
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms  :body")
+);
 
 let persons = [
   {
@@ -102,7 +108,6 @@ app.delete("/api/persons/:id", (request, response) => {
   response.status(204).end();
 });
 
-const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
